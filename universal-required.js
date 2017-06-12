32fab9db7c177255e994bb1ddedb6f9a1dd55117
auntphil <andhochmuth@gmail.com>
2017-06-12 10:44:44 -0400

    Checkboxes and Radio Groups are now quicker to check and use the same code.

--- universal-required.js
+++ universal-required.js
@@ -2,98 +2,57 @@
 
 //Runs the onLoad function as soon as the page is loaded. 
 window.onload = onLoad;
+var curRadio;
+var curCheck;
 
 function getFormValues(formId) {
+
 	document.getElementById('error_message').style.display = 'none';
     var form = document.getElementById(formId);//Selects the Form
 	var error = false; //Sets Error to false
 	var type = ''; //Sets Error to false
 	var length = form.length;
-	var curRadio;
-	var curCheck;
+	curRadio = '';
+	curCheck = '';
+	
+	var name;
+	var nameLength;
+	var nameElements;
+	
+	var required = [];
 	
 	//Loops through each control in the form
     for (var i=0, length; i<length; i++) {
-		form[i].style.outline = 'initial';
-		/*******************************************
-		 *Checking if a field is Number or Letter Only
-		 *******************************************/	
-		if(hasClass(form[i],'AlphaNumb')){					//Checking for AlphaNumberic Class
-			if(!/^[a-zA-Z0-9\s]+$/.test(form[i].value)){	//If the Field has anything other than Alpha Numeric and Space
-				if(form[i].value != '')						//Allows for the field to be blank
-				{
-					type="AlphaNumberic"		//Error Type
-					error=true;					//Error
-					break;						//Ends Loop
-				}
-			}
-		}
+		//checkAlphaNumeric(form, i);
 		
-		if(hasClass(form[i],'required'))//Checking if the control is required
+		form[i].style.outline = 'initial';
+		
+		/*Checks if the control is required*/
+		if(hasClass(form[i],'required'))
 		{ //The control is required
-			if(form[i].value == "") //Checking if the control value is blank
-			{ //Control value is blank and required
+			/*If Blank*/
+			if(form[i].value == "")
+			{
 				error = true; //Sets error to true
 				break; //Breaks out of the loop. There is no reason to search for more errors. 
 			}
 			
-			if(form[i].type == "radio") //Checking if the control is a radio button
+			/*If Control is a radio*/
+			if(form[i].type == "radio") 
 			{ //control is a radio button
-			
-				if(curRadio == form[i].name) //Checking if the radio has already been checked. 
-				{ //Already been checked. Skipping this iteration
-					continue;
-				}else{ //Not checked before. Adding radio to current.
-					curRadio = form[i].name;
-				}//else
-				
-				for (var j=0, length; j<length; j++) { //Loops through each control in the form
-					if(form[j].type == "radio") //Checking if radio
-					{ //Radio
-						if(form[j].name == curRadio) //Check agains form[i]
-						{
-							if(form[j].checked)
-							{
-								error = false;
-								break;
-							}else{
-								error = true;
-							} //if
-						}//if
-					}//if
-				}//for
-				if(error == true)
+				if(checkboxRadio(true,form,i)){
+					error = true;
 					break;
+				}
 			}//if
 			
 			
 			if(form[i].type == "checkbox") //Checking if the control is a checkbox
-			{ //control is a checkbox			
-			
-				if(curCheck == form[i].name) //Checking if the checkbox has already been checked. 
-				{ //Already been checked. Skipping this iteration
-					continue;
-				}else{ //Not checked before. Adding checkbox to current.
-					curCheck = form[i].name;
-				}//else
-				
-				for (var j=0, length; j<length; j++) { //Loops through each control in the form
-					if(form[j].type == "checkbox") //Checking if checkbox
-					{ //checkbox
-						if(form[j].name == curCheck) //Check agains form[i]
-						{
-							if(form[j].checked)
-							{
-								error = false;
-								break;
-							}else{
-								error = true;
-							} //if
-						}//if
-					}//if
-				}//for
-				if(error == true)
+			{
+				if(checkboxRadio(false,form,i)){
+					error=true;
 					break;
+				}
 			}//if
 		}//if
     }//for
@@ -156,3 +115,57 @@ function stringTest(elem)
 		elem.value = elem.value.replace(/[<>!$?\\//'"&]/g,'');
 	}
 }
+
+/******************************************************
+ Checking if the Checkbox OR Radio is Checked and Required
+******************************************************/
+function checkboxRadio(type,form,i){
+	/*Checking if the Radio Group has been checked before*/
+	if(curRadio == form[i].name || curCheck == form[i].name){
+		return false;	//The Group of Radio Buttons has already been checked. This iteration can be skipped.
+	}else{
+		if(type){
+			curRadio = form[i].name;						//Getting the Name of the current Radio Button
+			name = curRadio;								//Setting the name as the same as the curRadio	
+		}else{
+			curCheck = form[i].name;						//Getting the Name of the current Radio Button
+			name = curCheck;								//Setting the name as the same as the curRadio	
+		}
+		nameElements = document.getElementsByName(name);	//Getting a list of all elements with the same name
+		nameLength = nameElements.length;					//Getting the length of the list
+	}//else
+	
+	/*Loops through each radio button in this group*/
+	for (var j=0, nameLength; j<nameLength; j++) {
+		if(nameElements[j].checked){
+			error = false;	//Checked. No Error
+			break;			//Break Loop
+		}else{
+			error = true;	//Not Checked. Error Continue Loop
+		} //if
+	}//for
+	/*Is there an error*/
+	if(error)
+		return true;
+	else
+		return false;
+}
+/******************************************************
+ Checking if the Input is Alpha Numeric Only
+******************************************************/
+function checkAlphaNumeric(form, i){
+	/*******************************************
+	 *Checking if a field is Number or Letter Only
+	 *******************************************/	
+	if(hasClass(form[i],'AlphaNumb')){					//Checking for AlphaNumberic Class
+		if(!/^[a-zA-Z0-9\s]+$/.test(form[i].value)){	//If the Field has anything other than Alpha Numeric and Space
+			if(form[i].value != '')						//Allows for the field to be blank
+			{
+				type="AlphaNumberic"		//Error Type
+				error=true;					//Error
+				break;						//Ends Loop
+			}
+		}
+	}
+}
+
